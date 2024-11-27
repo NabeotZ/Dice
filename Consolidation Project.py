023e0ad72ecbc2c0abd64e6829cf512db1b97397
Nabeot Zemene <nabeotz@gmail.com>
2024-11-27 18:36:38 -0500

    Different results function

--- Consolidation Project.py
+++ Consolidation Project.py
@@ -17,3 +17,23 @@ def roll():
     third_dice = random.randint(1, 6)
     return [first_dice, second_dice, third_dice]
 
+#Find the dice with the different result
+def different_results(list_of_dice_rolls):
+    #For example, (5, 5, 5)
+    if list_of_dice_rolls[0] == list_of_dice_rolls[1] == list_of_dice_rolls[2]:  #All three have the same number
+        return 0
+    #For example, (2, 2, 5)
+    elif list_of_dice_rolls[0] == list_of_dice_rolls[1]:  #Third dice has a different result
+        return list_of_dice_rolls[2], 2
+    #For example, (1, 5, 1)
+    elif list_of_dice_rolls[0] == list_of_dice_rolls[2]:  #Second dice has a different result
+        return list_of_dice_rolls[1], 1
+    #For example, (5, 3, 3)
+    elif list_of_dice_rolls[1] == list_of_dice_rolls[2]:  #First dice has a different result
+        return list_of_dice_rolls[0], 0
+    #All three dice have different results
+    #For example, (1, 2, 3)
+    else:  
+        return "Dice are different", list_of_dice_rolls
+
+
